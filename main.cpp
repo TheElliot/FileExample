b1f836a6e347742b6b69219e2a5b48c5c9fecfb9
christeluwgb <christel@uwgb.edu>
2020-10-05 20:12:40 +0000

    typeo's

--- main.cpp
+++ main.cpp
@@ -8,22 +8,22 @@ using namespace std;
 
 void fileRead(string filename) {
   ifstream example_file;
-  example_file.open (filename);
+  example_file.open(filename);
   cout << "\nContents of file " << filename << ":\n\n";
   if (example_file.is_open()) {
     string line;
-    while ( getline (example_file,line) ) {
+    while ( getline(example_file, line) ) {
       cout << line << '\n';
     }
     example_file.close();
   } else {
-    cout << "Unable to open INPUT file"; 
+    cerr << "Unable to open INPUT file"; 
   }
 }
 
 void fileWrite(string filename) {
   ofstream example_file;
-  example_file.open (filename);
+  example_file.open(filename);
   
   if (example_file.is_open()) {
     example_file << "This is a random number: ";
@@ -32,13 +32,13 @@ void fileWrite(string filename) {
     example_file << (rand() % 100) << "\n";
     example_file.close();
   } else {
-    cout << "Unable to open OUTPUT file"; 
+    cerr << "Unable to open OUTPUT file"; 
   }
 }
 
 int main () {
   // seed random number generator
-  srand (time(NULL));
+  srand(time(NULL));
   string example_text_file = "./example.txt";
   fileRead(example_text_file);
   fileWrite(example_text_file);
